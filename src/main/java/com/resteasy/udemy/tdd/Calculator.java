f0f069a2dbe0193fcf56cbc5055d76618081bc27
Ant Brown <abbeydale@gmail.com>
2016-04-04 13:17:42 +0100

    Implement first cut of method - Now compiles, but test fails

---
+++ src/main/java/com/resteasy/udemy/tdd/Calculator.java
@@ -0,0 +1,12 @@
+package com.resteasy.udemy.tdd;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by Ant Brown on 04/04/2016.
+ */
+public class Calculator {
+	public BigDecimal getAccumulator() {
+		return null;
+	}
+}
