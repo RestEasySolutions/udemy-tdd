b2295564c84d640f45cb2cb4cc326e543d165125
Ant Brown <abbeydale@gmail.com>
2016-04-04 13:54:40 +0100

    Implemented the stack using the xisting methods., but these cause the original tests to fail (as we are getting an EmptyStackException)

--- src/main/java/com/resteasy/udemy/tdd/Calculator.java
+++ src/main/java/com/resteasy/udemy/tdd/Calculator.java
@@ -1,19 +1,21 @@
 package com.resteasy.udemy.tdd;
 
 import java.math.BigDecimal;
+import java.util.Stack;
 
 /**
  * Created by Ant Brown on 04/04/2016.
  */
 public class Calculator {
-	private BigDecimal accumulator = BigDecimal.ZERO;
+	private Stack<BigDecimal> values = new Stack<BigDecimal>();
 
 	public BigDecimal getAccumulator() {
-		return accumulator;
+		return values.peek();
 	}
 
 	public void setAccumulator(BigDecimal value) {
-		accumulator = value;
+		values.pop();
+		values.push(value);
 	}
 
 	public void enter() {
