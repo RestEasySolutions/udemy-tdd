8c9f094665b6ae3e383451a883abc591180b939e
Ant Brown <abbeydale@gmail.com>
2016-04-04 13:35:35 +0100

    Red - green - refactor Now in green state, so look to refactor. main code seems okay, but we can perform a minor refactor on the test code by de-duplicating the creation of the Calculator and moving it into a setup method

--- src/test/java/com/resteasy/udemy/tdd/CalculatorTest.java
+++ src/test/java/com/resteasy/udemy/tdd/CalculatorTest.java
@@ -1,5 +1,6 @@
 package com.resteasy.udemy.tdd;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.math.BigDecimal;
@@ -10,15 +11,20 @@ import static org.junit.Assert.assertEquals;
  * Created by Ant Brown on 04/04/2016.
  */
 public class CalculatorTest {
+	Calculator calculator;
+
+	@Before
+	public void createCalculator() {
+		calculator = new Calculator();
+	}
+
 	@Test
 	public void testNewCalculatorHasAnAccumulatorOfZero() {
-		Calculator calculator = new Calculator();
 		assertEquals(BigDecimal.ZERO, calculator.getAccumulator());
 	}
 
 	@Test
 	public void testSettingAccumulatorValue() {
-		Calculator calculator = new Calculator();
 		BigDecimal value = new BigDecimal(23);
 		calculator.setAccumulator(value);
 		assertEquals(value, calculator.getAccumulator());
