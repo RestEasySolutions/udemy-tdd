3e5e48d00c06a6311a4a80a38ebfe1dfa4879443
Ant Brown <abbeydale@gmail.com>
2016-04-04 13:26:30 +0100

    Added new test that allows us to test the first method with variable values. This relies on a new method that hasn't been implemented ... so won't compile.

--- src/test/java/com/resteasy/udemy/tdd/CalculatorTest.java
+++ src/test/java/com/resteasy/udemy/tdd/CalculatorTest.java
@@ -15,4 +15,12 @@ public class CalculatorTest {
 		Calculator calculator = new Calculator();
 		assertEquals(BigDecimal.ZERO, calculator.getAccumulator());
 	}
+
+	@Test
+	public void testSettingAccumulatorValue() {
+		Calculator calculator = new Calculator();
+		BigDecimal value = new BigDecimal(23);
+		calculator.setAccumulator(value);
+		assertEquals(value, calculator.getAccumulator());
+	}
 }
