dc42e21262caba7642b1e50fbd65c4a173bb00ed
Ant Brown <abbeydale@gmail.com>
2016-04-04 13:09:19 +0100

    First test ... won't compile

---
+++ src/test/java/com/resteasy/udemy/tdd/CalculatorTest.java
@@ -0,0 +1,16 @@
+package com.resteasy.udemy.tdd;
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by Ant Brown on 04/04/2016.
+ */
+public class CalculatorTest {
+	@Test
+	public void testNewCalculatorHasAnAccumulatorOfZero() {
+		Calculator calculator = new Calculator();
+		assertEquals(BigDecimal.ZERO, calculator.getAccumulator());
+	}
+}
