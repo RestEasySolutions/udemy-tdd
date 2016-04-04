a3aa9c2928c298f64c22c3b1a7e3a0822a4936ac
Ant Brown <abbeydale@gmail.com>
2016-04-04 15:12:34 +0100

    Refactored tests into two classes based on the state of the Calculator ... not sure that I'd do this

--- src/test/java/com/resteasy/udemy/tdd/CalculatorWithThreeValuesTest.java
+++ src/test/java/com/resteasy/udemy/tdd/CalculatorWithThreeValuesTest.java
@@ -10,42 +10,33 @@ import static org.junit.Assert.assertEquals;
 /**
  * Created by Ant Brown on 04/04/2016.
  */
-public class CalculatorTest {
+public class CalculatorWithThreeValuesTest {
+
 	Calculator calculator;
+	BigDecimal value1;
+	BigDecimal value2;
+	BigDecimal value3;
 
 	@Before
 	public void createCalculator() {
 		calculator = new Calculator();
-	}
-
-	@Test
-	public void testNewCalculatorHasAnAccumulatorOfZero() {
-		assertEquals(BigDecimal.ZERO, calculator.getAccumulator());
-	}
-
-	@Test
-	public void testSettingAccumulatorValue() {
-		BigDecimal value = new BigDecimal(23);
-		calculator.setAccumulator(value);
-		assertEquals(value, calculator.getAccumulator());
-	}
-
-	@Test
-	public void testEnteringAndDroppingValues() {
-		BigDecimal value1 = new BigDecimal(12);
-		BigDecimal value2 = new BigDecimal(22);
-		BigDecimal value3 = new BigDecimal(52);
+		value1 = new BigDecimal(12);
+		value2 = new BigDecimal(22);
+		value3 = new BigDecimal(52);
 		calculator.setAccumulator(value1);
 		calculator.enter();
 		calculator.setAccumulator(value2);
 		calculator.enter();
 		calculator.setAccumulator(value3);
+	}
+
+	@Test
+	public void testEnteringAndDroppingValues() {
 		assertEquals(value3, calculator.getAccumulator());
 		calculator.drop();
 		assertEquals(value2, calculator.getAccumulator());
 		calculator.drop();
 		assertEquals(value1, calculator.getAccumulator());
 	}
-
 
 }
---
+++ src/test/java/com/resteasy/udemy/tdd/InitialCalculatorStateTest.java
@@ -0,0 +1,34 @@
+package com.resteasy.udemy.tdd;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Ant Brown on 04/04/2016.
+ */
+public class InitialCalculatorStateTest {
+	Calculator calculator;
+
+	@Before
+	public void createCalculator() {
+		calculator = new Calculator();
+	}
+
+	@Test
+	public void testNewCalculatorHasAnAccumulatorOfZero() {
+		assertEquals(BigDecimal.ZERO, calculator.getAccumulator());
+	}
+
+	@Test
+	public void testSettingAccumulatorValue() {
+		BigDecimal value = new BigDecimal(23);
+		calculator.setAccumulator(value);
+		assertEquals(value, calculator.getAccumulator());
+	}
+
+
+}
