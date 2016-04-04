e24d3f4f07b895ebaa929079233352def7f14f51
Ant Brown <abbeydale@gmail.com>
2016-04-04 13:30:55 +0100

    Implemented methods correctly, so new test passes, but it has broken initial test (which always expects a zero)

--- src/main/java/com/resteasy/udemy/tdd/Calculator.java
+++ src/main/java/com/resteasy/udemy/tdd/Calculator.java
@@ -6,10 +6,13 @@ import java.math.BigDecimal;
  * Created by Ant Brown on 04/04/2016.
  */
 public class Calculator {
+	private BigDecimal accumulator;
+
 	public BigDecimal getAccumulator() {
-		return BigDecimal.ZERO;
+		return accumulator;
 	}
 
 	public void setAccumulator(BigDecimal value) {
+		accumulator = value;
 	}
 }
