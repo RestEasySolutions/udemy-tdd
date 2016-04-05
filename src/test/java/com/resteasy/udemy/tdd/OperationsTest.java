ef2fb7cdf91b71bef1a82ffa9acd87a57381804c
Ant Brown <abbeydale@gmail.com>
2016-04-05 16:24:47 +0100

    Added unimplemented method, and new test fails

---
+++ src/test/java/com/resteasy/udemy/tdd/OperationsTest.java
@@ -0,0 +1,30 @@
+package com.resteasy.udemy.tdd;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Ant Brown on 05/04/2016.
+ */
+public class OperationsTest {
+
+	private Calculator calculator;
+
+	@Before
+	public void createCalculatorWithTwoValues() {
+		calculator = new Calculator();
+		calculator.setAccumulator(new BigDecimal(20));
+		calculator.enter();
+		calculator.setAccumulator(new BigDecimal(4));
+	}
+
+	@Test
+	public void testAdd() {
+		calculator.add();
+		assertEquals(new BigDecimal(24), calculator.getAccumulator());
+	}
+}
