a74a314a5b7971fc44968fc228711572e5733850
Ant Brown <abbeydale@gmail.com>
2016-04-05 13:39:26 +0100

    Basic implementation of tests, but still fail due to EmptyStackExceptions (as happened with the Calculator Stack)

--- src/main/java/com/resteasy/udemy/tdd/OperandStack.java
+++ src/main/java/com/resteasy/udemy/tdd/OperandStack.java
@@ -1,15 +1,19 @@
 package com.resteasy.udemy.tdd;
 
 import java.math.BigDecimal;
+import java.util.Stack;
 
 /**
  * Created by Ant Brown on 04/04/2016.
  */
 public class OperandStack {
+	private Stack<BigDecimal> values = new Stack<BigDecimal>();
+
 	public BigDecimal peek() {
-		return BigDecimal.ZERO;
+		return values.peek();
 	}
 
 	public void push(BigDecimal value) {
+		values.push(value);
 	}
 }
